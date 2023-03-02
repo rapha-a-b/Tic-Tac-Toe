c9439fbb9141b0338a30005ab7b993049c1de01f
rapha-a-b <119280461+rapha-a-b@users.noreply.github.com>
2023-03-02 10:43:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+let cells = document.querySelectorAll(".cellClick");
+let resetButton = document.querySelector("button");
+resetButton.addEventListener("click", function () {
+  resetBoard();
+});
+let turnsCounter = 0;
+
+let board = [];
+function resetBoard() {
+  board = [];
+  turnsCounter = 0;
+  for (let x = 0; x < cells.length; x++) {
+    cells[x].innerHTML = "";
+    cells[x].addEventListener("click", function () {});
+  }
+}
+// resetButton.addEventListener(resetBoard());
+
+let winOptions = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [3, 5, 7],
+  [1, 5, 9],
+];
+let turns = 9;
+
+function gamePlay(num) {
+  if (turnsCounter % 2 === 0 && typeof board[num] !== "string") {
+    turnsCounter++;
+    sign = "❌";
+    board[num] = sign;
+    cells[num - 1].innerHTML = sign;
+  } else {
+    if (typeof board[num] !== "string") {
+      turnsCounter++;
+      sign = "⭕";
+      board[num] = sign;
+      cells[num - 1].innerHTML = sign;
+    }
+    setTimeout(function () {
+      checkWin(sign);
+    }, 500);
+  }
+}
+
+function checkWin(symbol) {
+  for (let x of winOptions) {
+    let isWinCount = 0;
+
+    for (let y of x) {
+      if (board[y] === symbol) {
+        isWinCount++;
+        console.log(isWinCount);
+        if (isWinCount === 3) {
+          alert(`${symbol} wins!`);
+          resetBoard();
+        }
+      } else {
+        isWinCount = 0;
+      }
+    }
+  }
+}
